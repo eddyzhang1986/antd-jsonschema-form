1b2a46001eb9878ad490a6c8acdeb88fd92d97af
eddyzhang1986 <eddyzhang1986@msn.cn>
2017-06-12 22:53:44 +0800

    inital this project

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,36 @@
 var webpack = require("webpack");
 
 module.exports = {
-    entry: ['babel-polyfill', './src/index.jsx'],
-    // node: {
-    //     setImmediate: true
-    // },
-    output: {
-        path: "./dist/",
-        publicPath: "/dist/",
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" }
-        ]
+  entry: ['babel-polyfill', './src/index.jsx'],
+  // node: {
+  //     setImmediate: true
+  // },
+  output: {
+    path: "./dist/",
+    publicPath: "/dist/",
+    filename: "bundle.js",
+    library: "AntdJSONSchemaForm",
+    libraryTarget: "umd"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    })
+  ],
+  devtool: "source-map",
+  externals: {
+    react: {
+      root: "React",
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react"
     }
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
 }
