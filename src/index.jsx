21bf305e223bfbbc47f54edf5f179f866e197410
eddyzhang1986 <eddyzhang1986@msn.cn>
2017-06-12 14:32:40 +0800

    update  export component

--- src/index.jsx
+++ src/index.jsx
@@ -8,16 +8,32 @@ const schema = {
   type: "object",
   required: ["title"],
   properties: {
-    title: {type: "string", title: "Title", default: "A new task"},
-    done: {type: "boolean", title: "Done?", default: false}
+    title: { type: "string", title: "Title", default: "A new task" },
+    done: { type: "boolean", title: "Done?", default: false }
   }
 };
 
 const log = (type) => console.log.bind(console, type);
 
+
+/**
+ * 导出测试发布到npm
+ */
+export default class FormTest extends Component {
+
+  render() {
+    return <Form schema={schema}
+      onChange={log("changed")}
+      onSubmit={log("submitted")}
+      onError={log("errors")} />
+  }
+
+}
+
+
 render((
   <Form schema={schema}
-        onChange={log("changed")}
-        onSubmit={log("submitted")}
-        onError={log("errors")} />
+    onChange={log("changed")}
+    onSubmit={log("submitted")}
+    onError={log("errors")} />
 ), document.getElementById("main"));
