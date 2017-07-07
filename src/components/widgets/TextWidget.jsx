6dff59895a400916252df81f7fcf152958a3a4f3
eddyzhang1986 <eddyzhang1986@msn.cn>
2017-07-07 17:10:57 +0800

    fix bug add .jsx extname

--- src/components/widgets/TextWidget.jsx
+++ src/components/widgets/TextWidget.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import BaseInput from "./BaseInput";
+
+import BaseInput from "./BaseInput.jsx";
 
 function TextWidget(props) {
   return <BaseInput {...props} />;
