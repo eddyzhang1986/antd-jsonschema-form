f949567f2ba8b4a59aa98424fcf2dfdd9e220b6c
eddyzhang1986 <eddyzhang1986@msn.cn>
2017-07-08 21:33:57 +0800

    fix bugs

---
+++ jest.js
@@ -0,0 +1,20 @@
+module.exports = {
+  setupFiles: [
+    './tests/setup.js',
+  ],
+  moduleFileExtensions: [
+    'jsx',
+    'json'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/'
+  ],
+  transform: {
+    '\\.jsx$': './node_modules/babel-jest'
+  },
+  snapshotSerializers: [
+    'enzyme-to-json/serializer',
+  ],
+  "coverageDirectory": "./coverage/",
+  "collectCoverage": true
+};
---
+++ test/setup.js
@@ -0,0 +1,12 @@
+﻿if (typeof window !== 'undefined') {
+  const matchMediaPolyfill = function matchMediaPolyfill() {
+    return {
+      matches false,
+      addListener() {
+      },
+      removeListener() {
+      },
+    };
+  };
+  window.matchMedia = window.matchMedia  matchMediaPolyfill;
+}
