aa05f1859380aac1d0723a57524483f15eaf3c7a
eddyzhang1986 <eddyzhang1986@msn.cn>
2017-07-10 11:59:30 +0800

    test async ()

--- test/components/Form.test.js
+++ test/components/Form.test.js
@@ -6,25 +6,25 @@ import { shallow } from 'enzyme'
 
 
 
- const setup = () => {
-  // mock props
-  const props = {
-    // Jest mock props
-  }
-  // enzyme test render
-  const wrapper = shallow(<Form {...props} />)
-  return {
-    props,
-    wrapper
-  }
+const setup = () => {
+    // mock props
+    const props = {
+        // Jest mock props
+    }
+    // enzyme test render
+    const wrapper = shallow(<Form {...props} />)
+    return {
+        props,
+        wrapper
+    }
 }
 
- describe('form', () => {
-  const { wrapper, props } = setup();
-  // assert
-  it('form Component should be render', () => {
-    //.find(selector) Enzyme
-    // div must be exists
-    expect(wrapper.find('div').text()).toEqual('test');
-  })
+describe('form', () => {
+    const { wrapper, props } = setup();
+    // assert
+    it('form Component should be render', async () => {
+        //.find(selector) Enzyme
+        // div must be exists
+        expect(wrapper.find('div').text()).toEqual('test');
+    })
 })
