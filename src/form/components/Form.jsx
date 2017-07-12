15363684372d16fa943e931ce700a93ad7d49bb6
eddyzhang1986 <eddyzhang1986@msn.cn>
2017-07-12 12:29:06 +0800

    重写jsonschema

--- src/form/components/Form.jsx
+++ src/form/components/Form.jsx
@@ -3,31 +3,94 @@ import PropTypes from "prop-types";
 
 
 
+
+const uiSchema = {
+    "xType": "grid",
+    "children": [{
+        "xType": "row",
+        "children": {
+            "xType": "field",
+            "props": {
+                "fPath": "/title"
+            }
+        }
+
+    }, {
+        "xType": "row",
+        "children": {
+            "xType": "field",
+            "props": {
+                "fPath": "/done"
+            }
+        }
+    }]
+
+}
+
+const schema = {
+
+    "type": "object",
+    "properties": {
+
+
+    }
+
+}
+
+
+const widgetMap = {
+
+
+
+}
+
+
+/**
+ * 
+ * @param {*} props 
+ */
+const VisitField = (props) => {
+    const { schema, uiSchema } = props;
+    const { fPath } = uiSchema;
+    const offSpringSchema = getOffspringSchema(schema, fPath);
+    return <Widget offSpringSchema={offSpringSchema} />
+}
+/**
+ * visit ui schema
+ * @param {*} props 
+ */
+const Visitor = (props) => {
+    const { schema, uiSchema } = props;
+    let result = null;
+    switch (uiSchema.xType) {
+
+        case "grid":
+            result = <VisitGrid schema={schema} uiSchema={uiSchema} />
+            break;
+        case "row":
+            result = <VisitRow schema={schema} uiSchema={uiSchema} />
+            break;
+        case "cls":
+            result = <VisitCls schema={schema} uiSchema={uiSchema} />
+            break;
+        case "tab":
+            result = <VisitTab schema={schema} uiSchema={uiSchema} />
+            break;
+        case "field":
+            result = <VisitField schema={schema} uiSchema={uiSchema} />
+            break;
+        default:
+            break;
+    }
+
+}
+
 export default class Form extends Component {
 
     render() {
-
-        //const { schema, uiSchema, formData, errorSchema, idSchema } = this.state;
-        //const registry = this.getRegistry();
-        //const _SchemaField = registry.fields.SchemaField;
-
         return <div>
-           test
+            <Visitor schema={schema}
+                uiSchema={uiSchema} />
         </div>
     }
-
-    getRegistry() {
-        // For BC, accept passed SchemaField and TitleField props and pass them to
-        // the "fields" registry one.
-        const { fields, widgets } = getDefaultRegistry();
-        return {
-            fields: { ...fields, ...this.props.fields },
-            widgets: { ...widgets, ...this.props.widgets },
-            ArrayFieldTemplate: this.props.ArrayFieldTemplate,
-            FieldTemplate: this.props.FieldTemplate,
-            definitions: this.props.schema.definitions || {},
-            formContext: this.props.formContext || {},
-        };
-    }
-    
 }
