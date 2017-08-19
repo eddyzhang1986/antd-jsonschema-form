190fac3bb033f8d3f7a5b258ac4b0398695e2c1c
eddyzhang1986 <eddyzhang1986@msn.cn>
2017-08-19 23:35:30 +0800

    add errors visitor

---
+++ src/form/components/visits/VisitError.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class VisitError extends Component {
+    render() {
+        return <div></div>
+    }
+
+}
+
+export default VisitError;
---
+++ src/form/components/visits/VisitErrorList.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class VisitErrorList extends Component {
+    render() {
+        return <div></div>
+    }
+
+}
+
+export default VisitErrorList;
